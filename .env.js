7e5fc47347c6d9c7ada6c4ad8425473f4842c516
vishal.maurya <vishal.maurya@globalsysinfo.com>
2022-11-05 00:58:49 +0530

    heroku chnage

--- .env.js
+++ .env.js
@@ -7,7 +7,7 @@ const development = {
     ...process.env,
     NODE_ENV: process.env.NODE_ENV || 'development',
     PORT: '9000',
-    Meta_WA_accessToken:'EAAJuWYqVosQBAGQ7OLGmKkZAK4DaDis3XnsL1esVCXwSZAmHMOm4Teq4czB0oZBwtHtDaZBUE97AUqIseTWUhRsgZBsHjdScDfeiRifIVEDbxz4PuoJP1NKUGjBNJL4ZC4AUCw88bOZAwyZCl27rw6iQYZCzZBVil1cv84LsLgtnli0ROuBVVCPbn8NLWmHrmUOlfOfogSkqhN9gZDZD',
+    Meta_WA_accessToken:'EAAJuWYqVosQBALUrJrhM2GpIIIToikrfFmoIHDZBMZCr7BOto8jGVcPEKqMt0zZC28KZAh4xWnBQS9CzLF5MR3RucBWD5absuS8cVbbWeYRMrRT2pHPWwiB7Uc0Tx99UOlDfZB2cOqPSgphW5MBAVPO3CRHqpoZCpskPfWlgMQCtJNv7ljvYwAv82KSqwNzUMVpqRpxGlkUAZDZD',
     Meta_WA_SenderPhoneNumberId: '105167829072955',
     Meta_WA_wabaId: '684280809628356',
     Meta_WA_VerifyToken: 'YouCanSetYourOwnToken',
