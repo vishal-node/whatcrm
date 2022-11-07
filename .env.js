2722a6999713c8b89c3e4727b2006b4c196d1dba
vishal.maurya <vishal.maurya@globalsysinfo.com>
2022-11-07 19:06:15 +0530

    added script in package json

--- .env.js
+++ .env.js
@@ -7,7 +7,7 @@ const development = {
     ...process.env,
     NODE_ENV: process.env.NODE_ENV || 'development',
     PORT: '9000',
-    Meta_WA_accessToken:'EAAJuWYqVosQBALUrJrhM2GpIIIToikrfFmoIHDZBMZCr7BOto8jGVcPEKqMt0zZC28KZAh4xWnBQS9CzLF5MR3RucBWD5absuS8cVbbWeYRMrRT2pHPWwiB7Uc0Tx99UOlDfZB2cOqPSgphW5MBAVPO3CRHqpoZCpskPfWlgMQCtJNv7ljvYwAv82KSqwNzUMVpqRpxGlkUAZDZD',
+    Meta_WA_accessToken:'EAAJuWYqVosQBADZB38OZCH7fuzYZCTEfDirhkgKXaeoF057R1WrCUXpHZBxPZAvGM3013vOZCe3axkAKha40yE7unV9aObVm7Gn5EEZA4CZBRd3XYDaJRZAQ7RhCYyt7FCz2OB6VnIZBh07Si6uhh7U5XNgcPp7tYDz9w1nrnaKA1sCOb3uLso3ZCfSzZCbIIavuZBP3FVfYBhiqNzQZDZD',
     Meta_WA_SenderPhoneNumberId: '105167829072955',
     Meta_WA_wabaId: '684280809628356',
     Meta_WA_VerifyToken: 'YouCanSetYourOwnToken',
